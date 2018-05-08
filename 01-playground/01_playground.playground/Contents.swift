f3c1ac8342b7a67394279a134d1ea54641144501
Роман <bartolomio@MacBook-Pro-Roman.local>
2018-05-08 20:20:43 +0300

    initial commit

---
+++ 01-playground/01_playground.playground/Contents.swift
@@ -0,0 +1,20 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// use optional in the homework
+
+class Recoord {
+    
+    let date: Date = Date()
+    
+    var name: String = ""
+    var text: String = ""
+    var tags: [String] = [String]()
+    
+    func description() -> String {
+        return ""
+    }
+}
+
+
