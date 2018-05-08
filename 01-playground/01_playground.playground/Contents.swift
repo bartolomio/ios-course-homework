cb4c1b1585d0379bbd8bb64a465aba7894bbe04f
bartolomio <bartolomioua@gmail.com>
2018-05-09 01:19:25 +0300

    Tabulated printouts implemented

--- 01-playground/01_playground.playground/Contents.swift
+++ 01-playground/01_playground.playground/Contents.swift
@@ -1,20 +1,53 @@
-//: Playground - noun: a place where people can play
-
 import UIKit
 
 // use optional in the homework
 
-class Recoord {
+class Record {
     
     let date: Date = Date()
     
-    var name: String = ""
-    var text: String = ""
+    var name: String?
+    var text: String?
     var tags: [String] = [String]()
     
     func description() -> String {
-        return ""
+        //Add data to result string
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateStyle = .long
+        dateFormatter.timeStyle = .none
+        dateFormatter.locale = Locale(identifier: "uk_UA")
+        var result: String = dateFormatter.string(from: self.date)
+        
+        //Add name to result string
+        if self.name != nil {
+            result = result + "\n" + self.name!
+        }
+        
+        //Add tags to result string
+        var tagString: String?
+        if tags.capacity > 0 {
+            tagString = ""
+            for tag in tags {
+                tagString = tagString! + "[" + tag + "]"
+            }
+            result = result + "\n" + tagString!
+        }
+        
+        //Add text to result string
+        if self.text != nil {
+            result = result + "\n" + self.text!
+        }
+        return result
     }
 }
+
+
+var record: Record = Record()
+
+//record.name = "ololo"
+//record.tags.append("1")
+//record.tags.append("2")
+record.text = "finish it"
+print (record.description())
 
 
