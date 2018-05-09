52188218174f47ac12a5059f7b96ec2062fad084
bartolomio <bartolomioua@gmail.com>
2018-05-09 12:26:52 +0300

    Initializers are added

--- 01-playground/01_playground.playground/Contents.swift
+++ 01-playground/01_playground.playground/Contents.swift
@@ -4,12 +4,33 @@ import UIKit
 
 class Record {
     
-    let date: Date = Date()
+    let date: Date
     
     var name: String?
     var text: String?
     var tags: [String] = [String]()
+    init(name: String? = nil, text: String? = nil, tags: String? = nil) {
+        self.date = Date()
+        
+        if let _: String = name {
+            self.name = name
+        }
+        if let _: String = text {
+            self.text = text
+        }
+        if let _: String = tags {
+            self.tags = tags!.components(separatedBy: ",")
+        }
+    }
     
+    convenience init? (date: Date, name: String? = nil, text: String? = nil, tags: String? = nil ) {
+        if date != Date() {
+            return nil
+        } else {
+            self.init(name: name, text: text, tags: tags)
+        }
+    }
+
     func description() -> String {
         //Add data to result string
         let dateFormatter = DateFormatter()
@@ -44,10 +65,13 @@ class Record {
 
 var record: Record = Record()
 
-//record.name = "ololo"
-//record.tags.append("1")
-//record.tags.append("2")
+record.name = "ololo"
+record.tags.append("1")
+record.tags.append("2")
 record.text = "finish it"
 print (record.description())
 
+var b: Record = Record(name:"lol", text:"123", tags:"1,2,3,4")
+print(b.description())
 
+var c: Record? = Record(date: Date(timeIntervalSince1970: 0), name: "222", text: "333", tags: "3,4,5")
