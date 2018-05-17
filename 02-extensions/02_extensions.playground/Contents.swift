3e7245cc40fc192972dfd9e37eb57ffa1d2026dc
bartolomio <bartolomioua@gmail.com>
2018-05-18 00:06:52 +0300

    Remove function added

--- 02-extensions/02_extensions.playground/Contents.swift
+++ 02-extensions/02_extensions.playground/Contents.swift
@@ -2,4 +2,18 @@
 
 import UIKit
 
+extension Array where Element: Equatable{
+    mutating func removeElement (element: Element){
+        
+        guard let index = self.index(of: element) else { return }
+        
+        self.remove(at: index)
+        
+    }
+}
+
+var a = [1,2,3,4,5,6,7]
+
+a.append(11)
+a.removeElement(element: 5)
 
