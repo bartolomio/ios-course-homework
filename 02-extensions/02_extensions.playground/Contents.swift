743519fdda6319fb4e562fe7c8d6ca4785702ed4
bartolomio <bartolomioua@gmail.com>
2018-05-18 22:55:21 +0300

    SafeRemoval method added

--- 02-extensions/02_extensions.playground/Contents.swift
+++ 02-extensions/02_extensions.playground/Contents.swift
@@ -10,10 +10,22 @@ extension Array where Element: Equatable{
         self.remove(at: index)
         
     }
+    
+    mutating func safeRemoval (index: Int){
+        if (index >= 0) && (index < self.count) {
+            self.remove(at: index)
+        }
+    }
 }
 
 var a = [1,2,3,4,5,6,7]
+print (a.description)
 
-a.append(11)
 a.removeElement(element: 5)
+print (a.description)
 
+a.safeRemoval(index: 5)
+print (a.description)
+
+a.safeRemoval(index: -3)
+print (a.description)
