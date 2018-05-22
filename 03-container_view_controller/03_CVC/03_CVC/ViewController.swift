1ea1e5e652959a372355602024235454d01264fb
bartolomio <bartolomioua@gmail.com>
2018-05-22 20:01:17 +0300

    HW3 initial commit

---
+++ 03-container_view_controller/03_CVC/03_CVC/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  03_CVC
+//
+//  Created by Роман on 22.05.18.
+//  Copyright © 2018 bartolomio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
