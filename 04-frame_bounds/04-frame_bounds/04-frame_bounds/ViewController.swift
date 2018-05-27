b6169b2df5cb34671894932d4e84e7febad861d8
bartolomio <bartolomioua@gmail.com>
2018-05-27 19:34:13 +0300

    HW4 initial commit

---
+++ 04-frame_bounds/04-frame_bounds/04-frame_bounds/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  04-frame_bounds
+//
+//  Created by Роман on 27.05.18.
+//  Copyright © 2018 bartolomio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
