24aab5a5381f4b654a4006f8edff7b22a9ab42ea
bartolomio <bartolomioua@gmail.com>
2018-05-27 23:23:23 +0300

    HW4 bullet2

--- 04-frame_bounds/04-frame_bounds/04-frame_bounds/ViewController.swift
+++ 04-frame_bounds/04-frame_bounds/04-frame_bounds/ViewController.swift
@@ -9,11 +9,43 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var tableView: UITableView = UITableView()
+    
+    var records: [Record] = [Record]()
 
+    @IBOutlet weak var editButton: UIButton!
+    
+    var conststrains: [NSLayoutConstraint] = [NSLayoutConstraint]()
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        //let tableView = UITableView(frame: <#T##CGRect#>)
+       
+        self.tableView.delegate = self
+        self.tableView.dataSource = self
+        self.tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        
+        records.append(Record(name: "name1", text: "text1", tags: "tag1,tag2"))
+        
+        
+        self.view.addSubview(tableView)
+        self.tableView.translatesAutoresizingMaskIntoConstraints = false
+        
+        tableView.leadingAnchor.constraint(equalTo: self.editButton.leadingAnchor).isActive = true
+        tableView.trailingAnchor.constraint(equalTo: self.editButton.trailingAnchor).isActive = true
+        tableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: self.editButton.topAnchor, constant: -40).isActive = true
+        
+        /*
+        let bottomAnchor = NSLayoutConstraint(item: tableView, attribute: .bottom, relatedBy: .equal, toItem: editButton, attribute: .top, multiplier: 1.0, constant: -40.0)
+        
+        let leadingAnchor = NSLayoutConstraint(item: tableView, attribute: .leading, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .leading, multiplier: 1, constant: 0)
+        let trailingAnchor = NSLayoutConstraint(item: tableView, attribute: .trailing, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .trailing, multiplier: 1, constant: 0)
+        let topAnchor = NSLayoutConstraint(item: tableView, attribute: .top, relatedBy: .equal, toItem: self.view.safeAreaLayoutGuide, attribute: .top, multiplier: 1, constant: 0)
+        self.view.addConstraints([bottomAnchor,leadingAnchor,trailingAnchor,topAnchor])
+        */
+        super.viewDidLoad() 
+        
+      
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -22,5 +54,29 @@ class ViewController: UIViewController {
     }
 
 
+}
+
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return records.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let record = records[indexPath.row]
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")!
+        cell.textLabel?.numberOfLines = 4
+        cell.textLabel?.text = record.description()
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //editButton.isEnabled = true
+    }
+    
+    
+    
 }
 
