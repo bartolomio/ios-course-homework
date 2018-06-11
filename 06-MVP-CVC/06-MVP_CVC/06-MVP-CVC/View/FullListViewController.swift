208fc4d60afad25f48ab39cc429ed3591886b492
bartolomio <bartolomioua@gmail.com>
2018-06-12 00:58:52 +0300

    TabBar icons added

--- 06-MVP-CVC/06-MVP_CVC/06-MVP-CVC/View/FullListViewController.swift
+++ 06-MVP-CVC/06-MVP_CVC/06-MVP-CVC/View/FullListViewController.swift
@@ -11,6 +11,8 @@ import UIKit
 class FullListViewController: UIViewController {
 
     var fullListTableView: UITableView = UITableView()
+    var action: Action = Action.no_Action
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,7 +27,35 @@ class FullListViewController: UIViewController {
         fullListTableView.trailingAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.trailingAnchor).isActive = true
         fullListTableView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor).isActive = true
         fullListTableView.bottomAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(reloadTableView), name: NSNotification.Name(rawValue: "record_edited"), object: nil)
 
+    }
+    
+    @objc func reloadTableView(notification: Notification) {
+        if notification.name.rawValue == "record_edited" {
+            print("[LOG MESSAGE] Notification received")
+            self.fullListTableView.reloadData()
+        }
+    }
+    
+    @IBAction func onAddButtonTapped(_ sender: Any) {
+        self.action = Action.add_Record
+        self.performSegue(withIdentifier: "toActionView", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let actionViewController = segue.destination as? ActionViewController {
+            actionViewController.delegate = self
+            actionViewController.action = self.action
+            if self.action == Action.edit_Record {
+                if let selectedRow = self.fullListTableView.indexPathForSelectedRow?.row {
+                    actionViewController.oldRecord = RecordHandler.shared.records[selectedRow]
+                    actionViewController.deleteButtonHiden = false
+                    actionViewController.deleteButtonEnabled = true
+                }
+            }
+        }
     }
 
 }
@@ -48,11 +78,30 @@ extension FullListViewController: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //self.action = Action.edit_Record
+        self.action = Action.edit_Record
         self.performSegue(withIdentifier: "toActionView", sender: self)
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return tableView.frame.height / 3
+    }
+}
+
+//MARK: ActionViewControllerDelegate conforming
+extension FullListViewController: ActionViewControllerDelegate{
+    
+    func addRecord(record: Record) {
+        RecordHandler.shared.records.append(record)
+        self.fullListTableView.beginUpdates()
+        let indexPath = IndexPath(row: RecordHandler.shared.records.count - 1, section: 0)
+        self.fullListTableView.insertRows(at: [indexPath], with: .automatic)
+        self.fullListTableView.endUpdates()
+    }
+    
+    func deleteRecord() {
+        if let selectedRow = self.fullListTableView.indexPathForSelectedRow?.row{
+            RecordHandler.shared.records.remove(at: selectedRow)
+        }
+        self.fullListTableView.reloadData()
     }
 }
